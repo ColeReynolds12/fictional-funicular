fdcfe3a0094a214b84ba537dc322089081789412
ColeReynolds12 <60250139+ColeReynolds12@users.noreply.github.com>
2020-08-14 15:17:19 -0500

    Update Pi.py

--- Pi.py
+++ Pi.py
@@ -1,14 +1,10 @@
-
-def num_steps(n, step_sizes):
-    if 0 in step_sizes:
-        return "0 cannot be a step size"
-    test = []
-    def recursion(n):
-        for x in step_sizes:
-            if x == n:
-                test.append(1)
-            elif x < n:
-                recursion(n-x)
-    recursion(n)
-    return len(test)
-print(num_steps(3,[1,2,3]))
+test = 0
+def recursion(n, step_sizes):
+    global test
+    for x in step_sizes:
+        if x == n:
+            test+=1
+        elif x < n:
+            recursion(n-x, step_sizes)
+    return test
+print(recursion(3, [1,2,3]))
